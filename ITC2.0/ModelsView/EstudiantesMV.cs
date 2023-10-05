74ea2af82c27e092eb3e74e0b43fb1500ef8468c
xHarald <jrreinosoronaldo@gmail.com>
2023-10-04 23:24:13 -0500

    LINQ
    Implementación de LINQ en Visual Studio 2022 | 04/10/2023, se crea el query que permite unir datos de diferentes tablas por el ID.

---
+++ ITC2.0/ModelsView/EstudiantesMV.cs
@@ -0,0 +1,19 @@
+﻿namespace ITC2._0.ModelsView
+{
+    public class EstudiantesMV
+    {
+        public int Codigo { get; set; }
+
+        public string? Nombre { get; set; }
+
+        public string? Telefono { get; set; }
+
+        public string? TipoIdentificacion { get; set; }
+
+        public int? Identificacion { get; set; }
+
+        public string? Programa { get; set; }
+
+        public string? Proyecto { get; set; }
+    }
+}
